9c32fb3a75d408caa38bc53af74ad3ebd0a6bc8e
marchdown <marchdown@gmail.com>
2011-11-12 22:13:08 +0000

    fixed registers.el

--- init.d/registers.el
+++ init.d/registers.el
@@ -4,7 +4,7 @@
 ;(set-register ?x '(file . "~/.xbindkeysrc.scm"))
 (set-register ?i '(file . "~/.emacs.d/init.el"))
 (set-register ?x '(file . "~/.xmonad/xmonad.hs"))
-(set-register ?r '(file . "~/.emacs.d/registers.el"))
+(set-register ?r '(file . "~/.emacs.d/init.d/registers.el"))
 (set-register ?t '(file . "~/org/tempus.org"))
 ;(set-register ?f '(file . "~/.emacs.d/funcs.el"))
 (set-register ?k '(file . "~/.ssh/known_hosts"))
