8a42aa1f6ce8fc34ad36cca1d703efb319ebd44b
marchdown <marchdown@gmail.com>
2014-12-21 05:02:00 +0300

    org-mode agenda files

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -10,9 +10,7 @@
    (quote
     ("987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "a233249cc6f90098e13e555f5f5bf6f8461563a8043c7502fb0474be02affeea" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "46fd293ff6e2f6b74a5edf1063c32f2a758ec24a5f63d13b07a20255c074d399" "1a85b8ade3d7cf76897b338ff3b20409cb5a5fbed4e45c6f38c98eee7b025ad4" default)))
  '(linum-format " %7i ")
- '(org-agenda-files
-   (quote
-    ("~/org/reading.org" "~/org/incubator.org" "~/org/README.org")))
+ '(org-agenda-files (quote ("~/Dropbox/org/commonplacebook.org")))
  '(package-archives (quote (("melpa" . "http://melpa.org/packages/"))))
  '(text-mode-hook (quote (text-mode-hook-identify))))
 (custom-set-faces
